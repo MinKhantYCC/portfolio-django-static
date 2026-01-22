39d1efbc21d26428ffc9ed2fe6ba28a4bc2f71ed
MinKhantYCC <linyaungzin99@gmail.com>
2026-01-23 01:40:58 +0700

    allowed hosts added

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -32,7 +32,9 @@ DEBUG = os.getenv("DEBUG", True)
 ALLOWED_HOSTS = [
     "127.0.0.1",
     ".vercel.app",
-    "portfolio-django-static.onrender.com"
+    "portfolio-django-static.onrender.com",
+    "minkhant.me",
+    "www.minkhant.me"
 ]
 
 
